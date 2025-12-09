18edfa99d7fe358d90dd57cb35ff80fdbe317625
Armaan Lala <armaanlala@gmail.com>
2025-12-08 18:06:10 -0800

    Configure Immich backup

--- modules/immich.nix
+++ modules/immich.nix
@@ -7,5 +7,12 @@
     port = 2283;
     openFirewall = true;
     host = "0.0.0.0";
+    mediaLocation = "/mnt/immich/media";
   };
+
+  # Ensure media directory exists with correct permissions
+  # TODO(human): Verify immich user/group ownership
+  systemd.tmpfiles.rules = [
+    "d /mnt/immich/media 0755 immich immich -"
+  ];
 }
