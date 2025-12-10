501bd210fabea7e21ad2feb6ebc6c5a62042f7d1
Armaan Lala <armaanlala@gmail.com>
2025-12-10 02:46:27 -0800

    Update thinkpad

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -4,10 +4,13 @@
 {
   imports = [
     ../../modules/common.nix
+    ../../modules/desktop.nix
     ./hardware-configuration.nix
   ];
 
   networking.hostName = "thinkpad";
+
+  services.tailscale.enable = true;
 
   system.stateVersion = "25.11";
 }
